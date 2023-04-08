0cc776e55dbd2eebc0c803c1b67c721faeb3dbff
TestKamml <129402004+TestKamml@users.noreply.github.com>
2023-04-08 21:31:57 +0200

    Update main.py
    das hab ich in git geändert

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # das weg
 
 
-# Press Umschalt+F10 to execute it or replace it with your code.
+# Press 
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
